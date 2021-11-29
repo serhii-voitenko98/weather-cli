aaa733512fea29056f5476e98cbc31f08ba0a248
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-11-29 22:22:01 +0200

    storage service

---
+++ services/storage.service.js
@@ -0,0 +1,37 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const isExist = async filePath => {
+    try {
+        await promises.stat(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+};
+
+const saveKeyValue = async (key, value) => {
+    let data = { [key]: value };
+
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        data = { ...JSON.parse(file), ...data };
+    }
+
+    return promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+    if (await isExist(filePath)) {
+        const fileString = await promises.readFile(filePath);
+        data = { ...JSON.parse(fileString) };
+        return data[key];
+    }
+
+    return null;
+};
+
+export { saveKeyValue, getKeyValue };
