e786e5c210c39d41a416fa94749b65e90ac9fc54
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-11-30 19:20:42 +0200

    implement get icon helper

---
+++ helpers/get-icon.js
@@ -0,0 +1,24 @@
+export const getWeatherIcon = iconCode => {
+    switch(iconCode.slice(0, -1)) {
+        case '01':
+            return '☀️';
+        case '02':
+            return '⛅';
+        case '03':
+            return '☁️';
+        case '04':
+            return '☁️';
+        case '09':
+            return '🌧️';
+        case '10':
+            return '🌦️';
+        case '11':
+            return '🌩️';
+        case '13':
+            return '❄️';
+        case '50':
+            return '🌫️';
+        default:
+            return '🌡️';
+    }
+};
